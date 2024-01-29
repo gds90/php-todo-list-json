144d4b5643077c980eec53ca53d4e61b06f29b9c
gds90 <you@example.com>
2024-01-29 16:50:56 +0100

    UPDATE when the user click on a task, it will be completed and appear line-through

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                     <div class="todolist-container">
                         <ul class="list-unstyled ">
                             <li v-for="todo, key in todoList" :key="key">
-                                <div class="todo_content" :class="todo.done ? 'text-decoration-line-through' : ''">
+                                <div class="todo_content" :class="todo.done ? 'text-decoration-line-through' : ''" @click="todoList[key].done = !todoList[key].done">
                                     <span>{{todo.name}}</span>
                                 </div>
                             </li>
