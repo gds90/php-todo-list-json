47f998643a6fbcd247a4e83b53f99a69f0943d68
gds90 <you@example.com>
2024-01-29 15:04:13 +0100

    Added todo-list.json and server.php files

--- index.php
+++ index.php
@@ -4,11 +4,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css
+    ">
     <link rel="stylesheet" href="css/style.css">
     <title>PHP To Do List</title>
 </head>
 
 <body>
+    <div id="app">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center my-3">
+                    <h1>To Do List</h1>
+                </div>
+                <div class="col-12">
+                    <ul class="list-unstyled ">
+                        <li v-for="todo, key in todoList" :key="key">
+                            {{todo.name}}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <input type="text" class="my-2 form-control " name="todoItem" placeholder="Inserisci nuovo To Do">
+                    <button class="btn btn-success ">Aggiungi</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.7/axios.min.js" integrity="sha512-NQfB/bDaB8kaSXF8E77JjhHG5PM6XVRxvHzkZiwl3ddWCEPBa23T76MuWSwAJdMGJnmQqM0VeY9kFszsrBEFrQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="js/script.js" type="text/javascript"></script>
