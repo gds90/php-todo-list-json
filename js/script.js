7ee1ca46fac525061aee8082ae16d65453bb8b9c
gds90 <you@example.com>
2024-01-29 15:12:55 +0100

    UPDATE get todo list from .json file using server.php as API endpoint

--- js/script.js
+++ js/script.js
@@ -3,13 +3,18 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            apiUrl: 'server.php',
+            todoList: []
         }
     },
     mounted() {
-
+        this.getTodoList();
     },
     methods: {
-
+        getTodoList() {
+            axios.get(this.apiUrl).then((response) => {
+                this.todoList = response.data;
+            })
+        }
     },
 }).mount('#app')
--- server.php
+++ server.php
@@ -1,1 +1,12 @@
 <?php
+// recupero il contenuto del file json
+$todoList = file_get_contents('todo-list.json');
+
+// decodifico la lista recuperata dal file json in array associativo
+$list = json_decode($todoList, true);
+
+// aggiungo all'header della risposta che sto passando un dato JSON
+header('Content-Type: application/json');
+
+//invio l'informazione in formato json
+echo json_encode($list);
